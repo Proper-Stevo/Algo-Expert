6c602836baef37ab72b4d24114e0f53daedf4639
Stevo <steven.barrios92@gmail.com>
2023-02-15 15:26:06 -0800

    next day question

---
+++ easy/nodesepths.js
@@ -0,0 +1,24 @@
+function nodeDepths(root) {
+    // Write your code here
+  return sumDepths(root, 0);
+}
+
+function sumDepths(node, level) {
+  if (!node) return 0;
+    return level + sumDepths(node.left, level + 1) + sumDepths(node.right, level + 1);
+  
+}
+  
+// This is the class of the input binary tree.
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// Do not edit the line below.
+exports.nodeDepths = nodeDepths;
+
+//link to question: https://www.algoexpert.io/questions/node-depths
